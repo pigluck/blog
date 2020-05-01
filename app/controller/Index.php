e580d15cb1af306555159f6ee55172d4de21ed1c
husong <1632382147@qq.com>
2020-05-01 15:37:58 +0800

    结构

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -3,11 +3,17 @@ namespace app\controller;
 
 use app\BaseController;
 use think\facade\View;
+use app\validate\index\User;
 
 class Index extends BaseController
 {
     public function index()
     {
+        try {
+            Validate(User::class)->check(['ll' => 11]);
+        }catch (\Exception $e){
+            var_dump($e->getMessage());
+        }
         return View::fetch('index');
     }
 
